a7a8abdb7d1ba3b6c177d244b970fd37ebe34fd8
Gleks <grzegorz.leks@pl.pwc.com>
2017-04-03 14:17:14 +0200

    1. Dodanie obrazka człowieka. 2. Lekka zmiana w strukturze HTML. 3. Kolejne style sekcji. 4. Podstawowa konfiguracja ścieżek w angularze.

--- src/bowen.angular.js
+++ src/bowen.angular.js
@@ -1,1 +1,12 @@
 var bowen = angular.module('bowen', ['ngRoute', 'ngSanitize']);
+
+bowen.config('$routeProvider', function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/src/Templates/home.html',
+            controller: 'HomeController'
+        })
+        .otherwise({
+            redirectTo: '/'
+        })
+});

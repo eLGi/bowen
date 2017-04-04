8d174b94c380876ac9988c5ec69cf0a66d88408c
Grzegorz Leks <ayats.bravo@gmail.com>
2017-04-04 20:18:06 +0200

    1. Dodanie ikonek; 2. Zmiana struktur folderów z obrazami. 3. Wszystkie style dla wersji dla desktopów przynajmniej z MDPI. 4. Podpięcie prawidłowe modułu Angulara.

---
+++ js/feedbacks.js
@@ -0,0 +1,11 @@
+var feedbacks = [{
+    id: 0,
+    author: 'Janina Kowalska',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt.',
+    feedback: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt.',
+}, {
+    id: 1,
+    author: 'Karol Kowalski',
+    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt.',
+    feedback: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt.',
+}]
--- src/bowen.angular.js
+++ src/bowen.angular.js
@@ -1,12 +1,13 @@
 var bowen = angular.module('bowen', ['ngRoute', 'ngSanitize']);
 
-bowen.config('$routeProvider', function($routeProvider) {
+bowen.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('');
     $routeProvider
         .when('/', {
-            templateUrl: '/src/Templates/home.html',
+            templateUrl: 'src/Templates/home.html',
             controller: 'HomeController'
         })
         .otherwise({
             redirectTo: '/'
         })
-});
+}]);

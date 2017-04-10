82cba99eacfaad6992a7323e086c5168708a5c70
Grzegorz Leks <ayats.bravo@gmail.com>
2017-04-10 19:38:52 +0200

    1. Podstrona o technice bowena. 2. Podstrona z treścią feedbacków. 3. Stylowanie.

--- src/Controllers/FeedbackController.js
+++ src/Controllers/FeedbackController.js
@@ -1,4 +1,8 @@
 bowen.controller('FeedbackController', ['$scope', '$location', function($scope, $location) {
+    //properties
     $scope.feedbackID = $location.search().id;
-    $scope.feedback = feedbacks[feedbackID];
+    $scope.feedback = feedbacks[$scope.feedbackID];
+    //On load
+    document.getElementById('viewStyle').href = '/css/views/feedback.css';
+    //Methods
 }]);

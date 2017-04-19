85db0b11094e7661d6ff0b07b1390a91c0924b28
Grzegorz Leks <ayats.bravo@gmail.com>
2017-04-19 18:00:26 +0200

    1. Poprawki od p. Tomasza.

--- js/feedbacks.js
+++ js/feedbacks.js
@@ -1,8 +1,15 @@
 var feedbacks = [{
     id: 0,
-    author: 'Janina Kowalska',
-    excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt.',
-    feedback: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempo incididont ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt.',
+    author: 'Filip K.',
+    excerpt: 'Po zakończeniu całego cyklu rehabilitacyjnego zarówno łokieć, jak i bark wróciły praktycznie do stanu sprzed urazu, a efekt utrzymuje się do dnia dzisiejszego.',
+    feedback: '<p>Z urazem łokcia borykałem się od wielu lat. Zdiagnozowano u mnie tzw. "łokieć tenisisty". Przechodziłem różnorakie rehabilitacje (wodne, elektryczne, naświetlania itp.) oraz zabiegi masująco-rozluźniające, łącznie z noszeniem szyny unieruchamiającej łokieć.</p>' +
+        '<p>Niestety, żadna z nich nie przyniosła oczekiwanego rezultatu.</p>' +
+        '<p>W 2016 roku zdecydowałem się skorzystać z usług Pana Tomasza Fica, który stosuje rehabilitację metodą Bowena.</p>' +
+        '<p>Po kilku zabiegach poczułem zdecydowaną poprawę. Ból w łokciu uległ zmniejszeniu, co pozwalało na w miarę swobodne wykorzystywanie go w codziennych czynnościach.</p>' +
+        '<p>Odbyłem kilkanaście zabiegów, również na bark, który też mi doskwierał, jednak w znacznie mniejszym zakresie.</p>' +
+        '<p>Po zakończeniu całego cyklu rehabilitacyjnego zarówno łokieć, jak i bark wróciły praktycznie do stanu sprzed urazu, a efekt utrzymuje się do dnia dzisiejszego.</p>' +
+        '<p>Uważam, że Pan Tomasz jest profesjonalistą w tym, co robi i zdecydowanie poleciłbym go osobom, które mają różne dolegliwości i tradycyjne metody się jak na razie nie sprawdziły.</p>' +
+        '<p>W razie pytań, można kontaktować się pod mailem: <a href="mailto:bluefil@gmail.com">bluefil@gmail.com</a>',
 }, {
     id: 1,
     author: 'Karol Kowalski',
